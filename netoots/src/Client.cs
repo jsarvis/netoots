60eae88ca7df48a8e1e37eebf508749f2a0aa499
Jacob Sarvis <jacob.sarvis@gmail.com>
2009-06-10 17:35:20 +0100

    Initial buildable source

---
+++ netoots/src/Client.cs
@@ -0,0 +1,24 @@
+/*=============================================================
+   Code Copyright 2009 Jacob Sarvis
+   All trademarks are the property of their respective owners.
+   OotS (and all characters, likenesses, and content) is the IP of Rich Burlew <www.GiantITP.com>,
+   used here (currently) without permission for non-commerical use.
+   The "Order of the Stick Adventure Game: Dungeon of Dorukan" is manufactured in part by APE Games <www.apegames.com/oots/oots-dod/oots-dod.html>.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+================================================================*/
+class Client{
+   static void Main() {
+      System.Console.WriteLine("Test");
+   }   
+}
